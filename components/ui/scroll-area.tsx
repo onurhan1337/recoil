958891ce53001b2e209770758f4db69db70d95a3
onurhan1337 <onurhandtr@gmail.com>
2025-11-23 16:54:21 +0300

    feat: add resizable panel and slider components, update package dependencies

--- components/ui/scroll-area.tsx
+++ components/ui/scroll-area.tsx
@@ -7,7 +7,20 @@ const ScrollArea = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("overflow-auto", className)}
+    className={cn(
+      "overflow-auto",
+      "[&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar]:h-2",
+      "[&::-webkit-scrollbar-track]:bg-transparent",
+      "[&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-muted/50 [&::-webkit-scrollbar-thumb]:border-2 [&::-webkit-scrollbar-thumb]:border-transparent [&::-webkit-scrollbar-thumb]:bg-clip-padding",
+      "[&::-webkit-scrollbar-thumb]:hover:bg-muted-foreground/40",
+      className
+    )}
+    style={
+      {
+        scrollbarWidth: "thin",
+        scrollbarColor: "hsl(var(--muted) / 0.5) transparent",
+      } as React.CSSProperties
+    }
     {...props}
   >
     {children}
